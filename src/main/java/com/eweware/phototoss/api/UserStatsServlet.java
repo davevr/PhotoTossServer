f18b8e65f68f33c1b826ab01d5a2575f90e4aa42
Dave Vronay <davevr@eweware.com>
2015-10-07 17:37:10 -0700

    added user stats

--- src/main/java/com/eweware/phototoss/api/UserStatsServlet.java
+++ src/main/java/com/eweware/phototoss/api/UserStatsServlet.java
@@ -1,20 +1,52 @@
 package com.eweware.phototoss.api;
 
+import com.eweware.phototoss.core.UserRecord;
+import com.eweware.phototoss.core.UserStatsRecord;
+import com.google.appengine.repackaged.com.google.api.client.http.HttpStatusCodes;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Created by ultradad on 10/5/15.
  */
 public class UserStatsServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
+    private static final Logger log = Logger.getLogger(UserStatsServlet.class.getName());
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
 
+        final UserRecord curUser = Authenticator.CurrentUser(session);
+
+        if (curUser != null) {
+            UserStatsRecord newRec = UserStatsRecord.CreateFromUser(curUser.id);
+
+            if (newRec != null) {
+                response.setContentType("application/json");
+
+                PrintWriter out = response.getWriter();
+                Gson gson = new GsonBuilder().create();
+                gson.toJson(newRec, out);
+                out.flush();
+                out.close();
+            } else {
+                // return not found
+                log.log(Level.SEVERE, "Unable to get stats of user %s", curUser.id);
+                response.setStatus(HttpStatusCodes.STATUS_CODE_NOT_FOUND);
+            }
+        } else {
+            // fail
+            log.log(Level.INFO, "Attempt to access photo stats when logged out");
+            response.setStatus(HttpStatusCodes.STATUS_CODE_FORBIDDEN);
+        }
     }
 }
--- src/main/java/com/eweware/phototoss/core/UserStatsRecord.java
+++ src/main/java/com/eweware/phototoss/core/UserStatsRecord.java
@@ -1,5 +1,9 @@
 package com.eweware.phototoss.core;
 
+import com.googlecode.objectify.Key;
+
+import static com.googlecode.objectify.ObjectifyService.ofy;
+
 /**
  * Created by ultradad on 10/5/15.
  */
@@ -9,4 +13,20 @@ public class UserStatsRecord {
     public Integer numtosses;
     public Integer numcatches;
     public Integer numimages;
+    public Integer numoriginals;
+
+    public static UserStatsRecord CreateFromUser(long curUserId) {
+
+
+        UserStatsRecord newRec = new UserStatsRecord();
+
+        newRec.userid = curUserId;
+        newRec.numtosses = ofy().load().type(TossRecord.class).filter("ownerId =", curUserId).count();
+        newRec.numcatches = ofy().load().type(PhotoRecord.class).filter("tosserid =", curUserId).count();
+
+        newRec.numimages = ofy().load().type(PhotoRecord.class).filter("ownerid =", curUserId).count();
+        newRec.numoriginals = ofy().load().type(PhotoRecord.class).filter("ownerid =", curUserId).filter("tossid =", 0).count();
+
+        return newRec;
+    }
 }
